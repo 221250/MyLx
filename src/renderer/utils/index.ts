f8df118e0213a58c1c1882f6ba3be4588dd49594
NiSong <119845730+221250@users.noreply.github.com>
2025-07-07 10:24:57 +0800

    Update index.ts

--- src/renderer/utils/index.ts
+++ src/renderer/utils/index.ts
@@ -80,3 +80,23 @@ export const decodeName = (str: string | null = '') => {
   if (!str) return ''
   return new window.DOMParser().parseFromString(str, 'text/html').body.textContent
 }
+
+
+// 添加以下函数
+export const rgbToHex = (r: number, g: number, b: number): string => {
+  return '#' + [r, g, b].map(x => {
+    const hex = x.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }).join('')
+}
+
+export const hexToRgb = (hex: string): [number, number, number] | null => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return result
+    ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16),
+      ]
+    : null
+}
